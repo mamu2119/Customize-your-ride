79bb5f6d581fb55893d46bf406a6826e0c796343
mamu2119 <chaitanyapandya1985@gmail.com>
2025-03-17 21:07:05 +0530

    Add files via upload

---
+++ Customise your ride.py
@@ -0,0 +1,15 @@
+print("Select your ride:")
+print("1. Bike")
+print("2. Car")
+choice = 2
+if (choice == 2):
+    print("What type of car?")
+    print("1. Audi")
+    print("2. Ambasaddar 2.0")
+    choice1 = 1
+        if choice1 == 1 :
+            print("You have selected Audi")
+        else:
+            print("You have selescted Ambasaddar 2.0")
+else:
+    print("Wrong choice!")
